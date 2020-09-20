2a77f9aa425d4a82f4870ea22b416166ceef9da0
Vlad <vlad.sandulescu@gmail.com>
2020-09-20 20:36:35 +0200

    Update generate_imgfeat_tsv.sh

--- scripts/generate_imgfeat_tsv.sh
+++ scripts/generate_imgfeat_tsv.sh
@@ -1,7 +1,7 @@
 cd $HOME/playground/hmm
 
 source ~/anaconda3/etc/profile.d/conda.sh
-conda activate py_bua_p36
+conda activate bua
 
 python detectron2_mscoco_proposal_maxnms_hm.py --split 'train'
 python detectron2_mscoco_proposal_maxnms_hm.py --split 'dev'
